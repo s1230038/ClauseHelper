e9c46117d4081ec23ba1eecf114acddbd2cc2bc7
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-01-18 17:35:09 +0900

    [update] take ch. 5.2.10
    learningSPA\webAppInPractice\officialSample
    \ts-nextbook-app-main\jest.config.js

--- clause-helper-app/jest.config.js
+++ clause-helper-app/jest.config.js
@@ -5,6 +5,7 @@ const createJestConfig = nextJest({ dir: './' })
 const customJestConfig = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
   testEnvironment: 'jsdom',
 }
 
