511ee95b3a3e76e61061482592a317fffc6ffccd
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-01-18 19:23:02 +0900

    [update] secret lint when Git commits

--- githooks/pre-commit.sh
+++ githooks/pre-commit.sh
@@ -3,12 +3,23 @@
 echo 
 echo '--- Execute Git hook ---'
 echo 'For more information, see .git/hooks/pre-commit'
-echo 
+echo '*** npm script ***'
+cd clause-helper-app
+
+echo 'Secretlint checking files':
+npm run secretlint
+# If secretlint errors, stop the commit
+if [ $? -eq 1 ]; then
+    echo "secretlint failed. Commit stopped."
+    exit 1
+fi
 
 echo 'ESLint automatically formats files:'
-cd clause-helper-app
 npm run format
 
+cd ..
+echo '*** npm script completed ***'
+
 echo
-echo '--- Finish execuiting Git hook ---'
+echo '--- Completed execuiting Git hook ---'
 echo
