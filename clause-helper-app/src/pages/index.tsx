685baf94880311ac19a387e6e76693ffdb0330ce
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-01-30 19:32:44 +0900

    [update] coding

--- clause-helper-app/src/pages/index.tsx
+++ clause-helper-app/src/pages/index.tsx
@@ -7,28 +7,7 @@ function App() {
 
   useEffect(() => {
     const convertText = () => {
-      // 置換対象の条数を抽出
-      const kanjiClauseList = extractSections(text)
-      console.log(kanjiClauseList)
-      // 置換対象文字列と置換後文字列のペアの配列を作り、それを元にtextを置換する。
-      type KanjiClause2numClause = { kanjiClause: string; numClause: string }
-      const repTable: KanjiClause2numClause[] = [] // replacement table
-      for (const kanjiClause of kanjiClauseList) {
-        const kanjiNumList = findKanjiNumbers(kanjiClause)
-        const kanjiNum: string = kanjiNumList[0] // kanjiNumList[0]のみが存在すると想定
-        const numClause: string = '第' + kanji2number(kanjiNum) + '条'
-        repTable.push({ kanjiClause, numClause })
-      }
-      console.log(repTable)
-      // TODO: 全角数字を半角数字に変換
-      // TODO: 第n項の漢数字も数字に変換
-
-      // 置換処理
-      let converted = text
-      for (const target of repTable) {
-        converted = converted.replaceAll(target.kanjiClause, target.numClause)
-      }
-
+      const converted: string = replaceKanjiClause2Num(text)
       setConvertedText(converted)
     }
     convertText()
@@ -48,8 +27,48 @@ function App() {
   )
 }
 
-function extractSections(text: string): string[] {
-  const regex = /第[一二三四五六七八九十百千]+条/g
+type replacedTarget = { beginning: string; end: string }
+type KanjiClause2numClause = { kanjiClause: string; numClause: string }
+function replaceKanjiClause2Num(
+  origText: string,
+  ...targets: replacedTarget[]
+): string {
+  let converted = origText
+  for (const target of targets) {
+    // 置換対象を抽出
+    const kanjiClauseList: string[] = extractSections(
+      origText,
+      target.beginning,
+      target.end,
+    )
+    console.log(kanjiClauseList)
+    // 置換対象文字列と置換後文字列のペアの配列を作り、それを元にtextを置換する。
+    const repTable: KanjiClause2numClause[] = [] // replacement table
+    for (const kanjiClause of kanjiClauseList) {
+      const kanjiNumList = findKanjiNumbers(kanjiClause)
+      const kanjiNum: string = kanjiNumList[0] // kanjiNumList[0]のみが存在すると想定
+      const numClause: string =
+        target.beginning + kanji2number(kanjiNum) + target.end
+      repTable.push({ kanjiClause, numClause })
+    }
+    console.log(repTable)
+    // 置換処理
+    for (const target of repTable) {
+      converted = converted.replaceAll(target.kanjiClause, target.numClause)
+    }
+  }
+
+  // TODO: 全角数字を半角数字に変換
+
+  return converted
+}
+
+function extractSections(
+  text: string,
+  beginning: string,
+  end: string,
+): string[] {
+  const regex = new RegExp(beginning + '[一二三四五六七八九十百千]+' + end, 'g')
   const matches = [...new Set(text.match(regex))]
   return matches ? matches : []
 }
