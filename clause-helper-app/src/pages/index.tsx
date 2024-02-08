98865031d26979412dd980587c2ee427177c0016
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-02-08 15:05:21 +0900

    [update] Doing Step 2

--- clause-helper-app/src/pages/index.tsx
+++ clause-helper-app/src/pages/index.tsx
@@ -1,100 +1,35 @@
 import { kanji2number, findKanjiNumbers } from '@geolonia/japanese-numeral'
 import { useState, useEffect } from 'react'
 
-function App() {
-  const [text, setText] = useState('')
-  const [convertedText, setConvertedText] = useState('')
-
-  useEffect(() => {
-    const convertText = () => {
-      const converted: string = replaceKanjiClause2Num(
-        text,
-        { beginning: '第', end: '条' },
-        { beginning: '第', end: '項' },
-      )
-      setConvertedText(converted)
-    }
-    convertText()
-  }, [text])
-
+function ExpandAllParentheses() {
   return (
-    <div>
-      <textarea
-        id="text"
-        placeholder="法律の条文を入力"
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-      />
-      <button id="convert">算用数字に変換</button>
-      <textarea id="convertedText" value={convertedText} readOnly />
-    </div>
+    <>
+      <button id="ExpandAllParentheses">丸括弧を展開</button>
+    </>
   )
 }
 
-type replacedTarget = { beginning: string; end: string }
-type ReplacePair = { from: string; to: string }
-function replaceKanjiClause2Num(
-  origText: string,
-  ...targets: replacedTarget[]
-): string {
-  let converted = origText
-  for (const target of targets) {
-    // 置換対象を抽出
-    const kanjiClauseList: string[] = extractSections(
-      origText,
-      target.beginning,
-      target.end,
-    )
-    console.log(kanjiClauseList)
-    // replacement table
-    const repTable: ReplacePair[] = getKanjiClause2NumClauseTable(
-      kanjiClauseList,
-      target.beginning,
-      target.end,
-    )
-
-    console.log(repTable)
-    // 置換処理
-    for (const target of repTable) {
-      converted = converted.replaceAll(target.from, target.to)
-    }
-  }
-
-  // 全角数字を半角数字に変換
-  converted = replaceHankakuSuji2Num(converted)
-
-  return converted
+function CollapseAllParentheses() {
+  return (
+    <>
+      <button id="CollapseAllParentheses">丸括弧を短縮</button>
+    </>
+  )
 }
 
-function getKanjiClause2NumClauseTable(
-  kanjiClauseList: string[],
-  beginning: string,
-  end: string,
-): ReplacePair[] {
-  // 置換対象文字列と置換後文字列のペアの配列を作る
-  const repTable: ReplacePair[] = [] // replacement table
-  for (const kanjiClause of kanjiClauseList) {
-    const kanjiNumList = findKanjiNumbers(kanjiClause)
-    const kanjiNum: string = kanjiNumList[0] // kanjiNumList[0]のみが存在すると想定
-    const numClause: string = beginning + kanji2number(kanjiNum) + end
-    repTable.push({ from: kanjiClause, to: numClause })
-  }
-  return repTable
-}
+function ParenthesesChangeRange() {
+  return (
+    <>
+      <p>丸括弧の展開／縮小の範囲</p>
+      <label>
+        <input type="radio" value="allLevels" />
+        全階層
+      </label>
 
-function extractSections(
-  text: string,
-  beginning: string,
-  end: string,
-): string[] {
-  const regex = new RegExp(beginning + '[一二三四五六七八九十百千]+' + end, 'g')
-  const matches = [...new Set(text.match(regex))]
-  return matches ? matches : []
+      <label>
+        <input type="radio" value="oneLevel" />
+        １階層
+      </label>
+    </>
+  )
 }
-
-function replaceHankakuSuji2Num(text: string): string {
-  const fullNums = '０１２３４５６７８９'
-  return text.replace(/[０-９]/g, (m) => fullNums.indexOf(m).toString())
-}
-
-export default App
