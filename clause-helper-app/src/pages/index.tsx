6eee6f3062cd197a2a4d9b124c65ab6df2b017f0
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-02-08 17:41:09 +0900

    [update] complete Step 2

--- clause-helper-app/src/pages/index.tsx
+++ clause-helper-app/src/pages/index.tsx
@@ -33,3 +33,79 @@ function ParenthesesChangeRange() {
     </>
   )
 }
+
+function ParenthesesManipulator() {
+  return (
+    <>
+      <ParenthesesChangeRange />
+      <CollapseAllParentheses />
+      <ExpandAllParentheses />
+    </>
+  )
+}
+
+function CopyConvertedClause() {
+  return (
+    <>
+      <button id="CopyConvertedClause">コピー</button>
+    </>
+  )
+}
+
+function Manipulator() {
+  return (
+    <>
+      <CopyConvertedClause />
+      <ParenthesesManipulator />
+    </>
+  )
+}
+
+function ConvertedClause({ convertedText }) {
+  return (
+    <>
+      <textarea id="ConvertedClause" value={convertedText} readOnly />
+    </>
+  )
+}
+
+function InputClause({ originalText }) {
+  return (
+    <>
+      <textarea
+        id="InputClause"
+        placeholder="法律の条文を入力"
+        value={originalText}
+      />
+    </>
+  )
+}
+
+// dummy data
+const DUMMY_ORIGINAL_TEXT =
+  '第十九条の三　法第百三条の二第五項第二号（法第百三条の三第二項及び第百六条の九において準用する場合を含む。）に規定する政令で定める特別の関係にある者は、次に掲げる関係にある者（特定株主を除く。）とする。'
+
+const DUMMY_CONVERTED_TEXT =
+  '第19条の三　法第103条の二第5項第二号（法第103条の三第2項及び第106条の九において準用する場合を含む。）に規定する政令で定める特別の関係にある者は、次に掲げる関係にある者（特定株主を除く。）とする。'
+
+function ClauseViewHelper() {
+  const originalText: string = DUMMY_ORIGINAL_TEXT
+  const convertedText: string = DUMMY_CONVERTED_TEXT
+  return (
+    <>
+      <InputClause originalText={originalText} />
+      <ConvertedClause convertedText={convertedText} />
+      <Manipulator />
+    </>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <ClauseViewHelper />
+    </>
+  )
+}
+
+export default App
