5a9f790c584b04a035724e13c4b8f037e9760052
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-02-15 16:11:01 +0900

    [update] take Kansuji replace

--- clause-helper-app/src/pages/index.tsx
+++ clause-helper-app/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { kanji2number, findKanjiNumbers } from '@geolonia/japanese-numeral'
+import { useState, useEffect } from 'react'
 
 function ExpandAllParentheses() {
   return (
@@ -78,18 +79,28 @@ function InputClause({
   )
 }
 
-// dummy data
-const DUMMY_CONVERTED_TEXT =
-  '固定：第19条の三　法第103条の二第5項第二号（法第103条の三第2項及び第106条の九において準用する場合を含む。）に規定する政令で定める特別の関係にある者は、次に掲げる関係にある者（特定株主を除く。）とする。'
-
 // 型エイリアス (type alias)
 type RadioButtonOption = { value: string; displayLabel: string }
 type OnChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => void
 type OnChangeTextArea = (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+type replacedTarget = { beginning: string; end: string }
+type ReplacePair = { from: string; to: string }
 
 function ClauseViewHelper() {
   const [originalText, setOriginalText] = useState('')
-  const convertedText: string = DUMMY_CONVERTED_TEXT
+  const [convertedText, setConvertedText] = useState('')
+  useEffect(() => {
+    const convertText = () => {
+      const converted: string = replaceKanjiClause2Num(
+        originalText,
+        { beginning: '第', end: '条' },
+        { beginning: '第', end: '項' },
+      )
+      setConvertedText(converted)
+    }
+    convertText()
+  }, [originalText])
+
   const [selectedRange, setSelectedRange] = useState('allLevels')
   const rangeOptions: RadioButtonOption[] = [
     { value: 'allLevels', displayLabel: '全階層' },
@@ -118,6 +129,70 @@ function ClauseViewHelper() {
       <ExpandAllParentheses />
     </>
   )
+}
+
+function replaceKanjiClause2Num(
+  origText: string,
+  ...targets: replacedTarget[]
+): string {
+  let converted = origText
+  for (const target of targets) {
+    // 置換対象を抽出
+    const kanjiClauseList: string[] = extractSections(
+      origText,
+      target.beginning,
+      target.end,
+    )
+    console.log(kanjiClauseList)
+    // replacement table
+    const repTable: ReplacePair[] = getKanjiClause2NumClauseTable(
+      kanjiClauseList,
+      target.beginning,
+      target.end,
+    )
+
+    console.log(repTable)
+    // 置換処理
+    for (const target of repTable) {
+      converted = converted.replaceAll(target.from, target.to)
+    }
+  }
+
+  // 全角数字を半角数字に変換
+  converted = replaceHankakuSuji2Num(converted)
+
+  return converted
+}
+
+function getKanjiClause2NumClauseTable(
+  kanjiClauseList: string[],
+  beginning: string,
+  end: string,
+): ReplacePair[] {
+  // 置換対象文字列と置換後文字列のペアの配列を作る
+  const repTable: ReplacePair[] = [] // replacement table
+  for (const kanjiClause of kanjiClauseList) {
+    const kanjiNumList = findKanjiNumbers(kanjiClause)
+    const kanjiNum: string = kanjiNumList[0] // kanjiNumList[0]のみが存在すると想定
+    const numClause: string = beginning + kanji2number(kanjiNum) + end
+    repTable.push({ from: kanjiClause, to: numClause })
+  }
+  return repTable
+}
+
+function extractSections(
+  text: string,
+  beginning: string,
+  end: string,
+): string[] {
+  const regex = new RegExp(beginning + '[一二三四五六七八九十百千]+' + end, 'g')
+  const matches = [...new Set(text.match(regex))]
+  return matches ? matches : []
+}
+
+function replaceHankakuSuji2Num(text: string): string {
+  const fullNums = '０１２３４５６７８９'
+  return text.replace(/[０-９]/g, (m) => fullNums.indexOf(m).toString())
 }
 
 function App() {
