3fe39bc446659fea4af5f56a73255d35e794b440
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-02-20 16:33:11 +0900

    [update] append first UT

--- clause-helper-app/src/pages/ClauseViewHelper.spec.tsx
+++ clause-helper-app/src/pages/ClauseViewHelper.spec.tsx
@@ -1,18 +1,27 @@
 import { render, screen, RenderResult, fireEvent } from '@testing-library/react'
 import { ClauseViewHelper } from './ClauseViewHelper'
 
-describe('Input', () => {
+describe('Input Clause', () => {
   let renderResult: RenderResult
 
   // それぞれのテストケース前にコンポーネントを描画し、renderResultにセットする
   beforeEach(() => {
-    renderResult = render(
-      <ClauseViewHelper id="username" label="InputTextArea" />,
-    )
+    renderResult = render(<ClauseViewHelper />)
   })
 
   // テストケース実行後に描画していたコンポーネントを開放する
   afterEach(() => {
     renderResult.unmount()
+  })
+
+  // 初期描画時にinput要素が空であることをテスト
+  it('should empty in input on initial render', () => {
+    // labelがUsernameであるコンポーネントに対応するinputの要素を取得する
+    const inputNodes: HTMLInputElement[] = screen.getAllByRole('textbox')
+
+    // input要素の表示が空か確認する
+    for (const n of inputNodes) {
+      expect(n).toHaveValue('')
+    }
   })
 })
