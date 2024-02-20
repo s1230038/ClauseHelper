9f1e5cd75126dec02d884503a58934406527f538
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-02-20 15:54:47 +0900

    [update] rename

--- clause-helper-app/src/pages/ClauseViewHelper.spec.tsx
+++ clause-helper-app/src/pages/ClauseViewHelper.spec.tsx
@@ -1,12 +1,14 @@
 import { render, screen, RenderResult, fireEvent } from '@testing-library/react'
-import App from './index'
+import { ClauseViewHelper } from './ClauseViewHelper'
 
 describe('Input', () => {
   let renderResult: RenderResult
 
   // それぞれのテストケース前にコンポーネントを描画し、renderResultにセットする
   beforeEach(() => {
-    renderResult = render(<App label="InputTextArea" />)
+    renderResult = render(
+      <ClauseViewHelper id="username" label="InputTextArea" />,
+    )
   })
 
   // テストケース実行後に描画していたコンポーネントを開放する
