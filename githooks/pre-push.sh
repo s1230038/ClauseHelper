3205a01770c762834989c8cbd158cf755458c365
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-02-21 17:55:02 +0900

    [add] pre-push Git hook to run test

---
+++ githooks/pre-push.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo 
+echo '--- Execute Git hook ---'
+echo 'For more information, see .git/hooks/pre-push'
+echo
+echo '*** npm script ***'
+cd clause-helper-app
+
+echo 'Test runs'
+npm run test
+
+
+cd ..
+echo '*** npm script completed ***'
+
+echo
+echo '--- Completed execuiting Git hook ---'
+echo
