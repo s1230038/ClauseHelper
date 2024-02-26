6581345f2ea64842b93fa2c2b4b2c82d22a9b8c5
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-02-26 15:59:02 +0900

    [update] comments

--- clause-helper-app/src/pages/ClauseViewHelper.tsx
+++ clause-helper-app/src/pages/ClauseViewHelper.tsx
@@ -215,7 +215,8 @@ function replaceKanjiClause2Num(origText: string): string {
   let repTable: ReplacePair[] = getReplaceTableForBranchNumber(origText)
   repTable = repTable.concat(getReplaceTableForArticleAndParagraph(origText))
 
-  // repTableを文字数の長い要素から降順にソート
+  // repTableを文字数の長い要素から降順にソート。
+  // 長い置換対象文字列と短い置換対象文字列に重複する文字列がある場合、先に短い方の置換をすると長い方の置換が行われなくなるため。
   repTable.sort((a, b) => b.from.length - a.from.length)
 
   // 置換処理
