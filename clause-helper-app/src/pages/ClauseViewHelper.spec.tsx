9bd1a23e071fc93075aa7c020f55fd403135a68d
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-02-26 18:56:25 +0900

    [update] append test case

--- clause-helper-app/src/pages/ClauseViewHelper.spec.tsx
+++ clause-helper-app/src/pages/ClauseViewHelper.spec.tsx
@@ -4,6 +4,7 @@ import { render, screen, RenderResult, fireEvent } from '@testing-library/react'
 import {
   ClauseViewHelper,
   getParenthesisCorrespondence,
+  collapse,
 } from './ClauseViewHelper'
 
 describe('Input Clause', () => {
@@ -205,5 +206,17 @@ describe('Parenthesis Correspondence', () => {
 
     const result = getParenthesisCorrespondence(inputText)
     expect(result).toEqual(expectedCorrespondence)
+  })
+
+  it('should replace parenthesis blocks', () => {
+    const inputText =
+      '事業者（中間処理業者（発生から最終処分（埋立処分、海洋投入処分（海洋汚染等及び海上災害の防止に関する法律に基づき定められた海洋への投入の場所及び方法に関する基準に従つて行う処分をいう。）又は再生をいう。以下同じ。）が終了するまでの一連の処理の行程の中途において産業廃棄物を処分する者をいう。以下同じ。）を含む。次項及び第7項並びに次条第5項から第7項までにおいて同じ。）は、その産業廃棄物（特別管理産業廃棄物を除くものとし、中間処理産業廃棄物（発生から最終処分が終了するまでの一連の処理の行程の中途において産業廃棄物を処分した後の産業廃棄物をいう。以下同じ。）を含む。次項及び第7項において同じ。）の運搬又は処分を他人に委託する場合には、その運搬については第14条第12項に規定する産業廃棄物収集運搬業者その他環境省令で定める者に、その処分については同項に規定する産業廃棄物処分業者その他環境省令で定める者にそれぞれ委託しなければならない。'
+
+    const expected =
+      '事業者（中間処理業者（発生から最終処分（埋立処分、海洋投入処分（海洋汚染等及び海上災害の防止に関する法律に基づき定められた海洋への投入の場所及び方法に関する基準に従つて行う処分をいう。）又は再生をいう。以下同じ。）が終了するまでの一連の処理の行程の中途において産業廃棄物を処分する者をいう。以下同じ。）を含む。次項及び第7項並びに次条第5項から第7項までにおいて同じ。）は、その産業廃棄物（特別管理産業廃棄物を除くものとし、中間処理産業廃棄物（発生から最終処分が終了するまでの一連の処理の行程の中途において産業廃棄物を処分した後の産業廃棄物をいう。以下同じ。）を含む。次項及び第7項において同じ。）の運搬又は処分を他人に委託する場合には、その運搬については第14条第12項に規定する産業廃棄物収集運搬業者その他環境省令で定める者に、その処分については同項に規定する産業廃棄物処分業者その他環境省令で定める者にそれぞれ委託しなければならない。'
+
+    const pcList = getParenthesisCorrespondence(inputText)
+    const result = collapse(inputText, 0, pcList)
+    expect(result).toEqual(expected)
   })
 })
