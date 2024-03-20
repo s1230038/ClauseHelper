fd2fb5dd74bb101687d48ed57c426157aba2b5c2
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-03-20 18:28:49 +0900

    [add] introduce Google Analytics

--- clause-helper-app/src/pages/_app.tsx
+++ clause-helper-app/src/pages/_app.tsx
@@ -1,10 +1,43 @@
 import '@/styles/globals.css'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
+import { useEffect } from 'react'
+import * as gtag from 'src/lib/gtag'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouterChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouterChange)
+    }
+  }, [router.events])
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+           `,
+        }}
+      />
       <Component {...pageProps} />
       <SpeedInsights />
     </>
