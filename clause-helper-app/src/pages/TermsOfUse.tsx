8afbedf07b8102fc3c3f22608601358483406893
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-03-21 18:59:17 +0900

    [update] fix sentence

--- clause-helper-app/src/pages/TermsOfUse.tsx
+++ clause-helper-app/src/pages/TermsOfUse.tsx
@@ -6,7 +6,7 @@ function TermsOfUse() {
     <div className={styles.container}>
       <h1 className={styles.header}>免責事項</h1>
       <p>
-        本Webアプリケーションを利用した結果生じるいかなる損害についても、作者は責任を負いかねます。ユーザー自身の責任のもとでの利用をお願いいたします。
+        本Webアプリケーションを利用した結果生じるいかなる損害についても、作者は責任を負いかねます。ユーザー自身の責任で利用をお願いします。
       </p>
       <Link href="/">戻る</Link>
     </div>
