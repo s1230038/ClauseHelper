b708593e789184cccf1a11e170162b530ec27d1b
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-08-24 10:44:30 +0900

    [update] checking pre-push test

--- githooks/pre-push.sh
+++ githooks/pre-push.sh
@@ -6,7 +6,8 @@ echo 'For more information, see .git/hooks/pre-push'
 echo
 echo '*** npm script ***'
 cd clause-helper-app
-
+echo '* update lib in according with package-lock.json'
+npm ci
 echo '* Test runs'
 npm run test
 echo
